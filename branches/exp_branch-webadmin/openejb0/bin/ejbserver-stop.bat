3e651bae2db1d46e11e76df4dc11a21a7871f0fa
nobody <nobody@2b0c1533-c60b-0410-b8bd-89f67432e5c6>
2002-09-02 21:18:02 +0000

    This commit was manufactured by cvs2svn to create branch 'exp_branch-webadmin'.

---
+++ branches/exp_branch-webadmin/openejb0/bin/ejbserver-stop.bat
@@ -0,0 +1,19 @@
+@echo off
+REM $Id$
+
+REM   Contributions by:
+REM      David Blevins <david.blevins@visi.com>
+
+if "%OPENEJB_HOME%"=="" set OPENEJB_HOME=%CD%
+
+set JAVA=%JAVA_HOME%\bin\java
+
+set CP=
+for %%i in (%OPENEJB_HOME%\dist\openejb*.jar) do call cp.bat %%i
+set CP=%JAVA_HOME%\lib\tools.jar;%CP%
+
+set OPTIONS=-Dopenejb.home=%OPENEJB_HOME%
+
+%JAVA% %OPTIONS% -classpath %CLASSPATH% org.openejb.server.Stop  %1 %2 %3 %4 %5 %6
+
+

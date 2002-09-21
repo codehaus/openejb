76984839a791be1453900bb4e89b7e11c86582c3
dblevins <dblevins@2b0c1533-c60b-0410-b8bd-89f67432e5c6>
2002-09-21 19:28:28 +0000

    Putting the refactered config stuff in the webadmin branch

--- branches/exp_branch-webadmin/openejb0/src/facilities/org/openejb/alt/config/ConfigurationFactory.java
+++ branches/exp_branch-webadmin/openejb0/src/facilities/org/openejb/alt/config/ConfigurationFactory.java
@@ -1036,7 +1036,7 @@ public class ConfigurationFactory implements OpenEjbConfigurationFactory, Provid
             }
 
             try {
-                EjbJar ejbJar = ConfigUtils.readEjbJar(jarLocation);
+                EjbJar ejbJar = EjbJarUtils.readEjbJar(jarLocation);
                 /* If there is no openejb-jar.xml an exception
                  * will be thrown.
                  * TODO: This shouldn't cause such a problem.  If

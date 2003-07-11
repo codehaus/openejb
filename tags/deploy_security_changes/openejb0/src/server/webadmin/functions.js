5251ec741e8f566859c06600d88ba7c7d23f0316
nobody <nobody@2b0c1533-c60b-0410-b8bd-89f67432e5c6>
2003-07-11 16:55:26 +0000

    This commit was manufactured by cvs2svn to create tag 'deploy_security_changes'.

---
+++ tags/deploy_security_changes/openejb0/src/server/webadmin/functions.js
@@ -0,0 +1,54 @@
+/******************************************************************
+* Author: Tim Urberg - tim_urberg@yahoo.com 
+* Description: Contains JavaScript functions used by the webadmin 
+* Date: 11/23/2002
+*******************************************************************/
+
+//checks to make sure the jar file form field is filled in
+function checkDeploy(form)
+{
+    //set up local variables
+    var goodForm = true;
+    var msg = "";
+   
+    if(form.jarFile.value == "")
+    {
+       msg = "Please enter the full path to your jar file.";
+       goodForm = false;
+    }
+   
+    //check for form submission
+    if(!goodForm)
+    {
+        alert(msg);
+        return false;
+    }
+    
+    return true;
+}
+
+//checks to make sure the second page of the deploy is filled out
+function checkDeployValues(form)
+{
+	//convience variables
+	var formName;
+	var formValue;
+	
+	//loop through all elements of the array
+	for(var i=0; i<form.elements.length; i++)
+	{
+		formName = form.elements[i].name;
+		formValue = form.elements[i].value;
+		
+		if(formName.indexOf("Parameters") == -1)
+		{
+			if(formValue == "")
+			{
+				alert("All form variables (except OQL parameters) are required.");
+				return false;
+			}
+		}
+	}
+	
+	return true;
+}

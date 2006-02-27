04e4b25221c0c8440b8e732aac50c4fd47af4593
dblevins <dblevins@2b0c1533-c60b-0410-b8bd-89f67432e5c6>
2006-02-27 00:45:53 +0000

    Tag for 1.0

---
+++ tags/v1_0/modules/core/src/bin/openejb.bat
@@ -0,0 +1,26 @@
+@echo off
+REM================================================
+REM   Control script for OpenEJB
+REM   --------------------------
+REM    
+REM   This script is the central entry point to 
+REM   all of OpenEJB's functions.
+REM  
+REM   Tested on Windows 2000, Windows XP
+REM             Windows XP (Cygwin)
+REM
+REM
+REM   Created by Jeremy Whitlock
+REM             <jcscoobyrs@codehaus.org>
+REM _______________________________________________
+REM $Id$
+REM================================================
+
+SETLOCAL
+
+SET COMMAND=%0
+SET OPENEJB_HOME=%COMMAND:~0,-16%
+
+echo OPENEJB_HOME=%OPENEJB_HOME%
+
+java %JAVA_OPTS% -jar %OPENEJB_HOME%/lib/openejb-core-@REPLACED-BY-MAVEN-XML@.jar %*
